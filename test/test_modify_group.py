b065f6e1598ae8b50cae963e28d56b2b53c4dc9a
Natali Kucheryavaya <crispnik@gmail.com>
2020-09-09 19:43:24 +0300

    Added conftest, test_del_group, test_modify_group

---
+++ test/test_modify_group.py
@@ -0,0 +1,13 @@
+from model.group import Group
+
+
+def test_modify_first_group_name(app):
+    app.group.modify_first_group(Group(name="New group"))
+
+
+def test_modify_first_group_header(app):
+    app.group.modify_first_group(Group(header="New_header"))
+
+
+def test_modify_first_group_footer(app):
+    app.group.modify_first_group(Group(footer="New_footer"))
